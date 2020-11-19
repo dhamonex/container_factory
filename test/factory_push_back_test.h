e7d29fcd122d6719ce79999cf576658f0efc7261
Dirk Hartmann <2monex@gmx.net>
2020-11-19 20:07:16 +0100

    created more test cases

--- test/factory_push_back_test.h
+++ test/factory_push_back_test.h
@@ -63,6 +63,88 @@ TEST_F( ContainerFactoryStdSharedPtrVectorTest, add_two_base_elements )
   ASSERT_THAT( destinationContainer, SizeIs( 2 ) );
 }
 
+
+class ContainerFactoryBoostSharedPtrVectorTest : public Test
+{
+  protected:
+    using VectorContainer = std::vector<boost::shared_ptr<Base>>;
+    
+    void TearDown() override
+    {
+      destinationContainer.clear();
+    }
+    
+    VectorContainer destinationContainer;
+};
+
+TEST_F( ContainerFactoryBoostSharedPtrVectorTest, add_one_base_element )
+{
+  container_factory::factory<Base>( destinationContainer );
+  
+  ASSERT_THAT( destinationContainer, SizeIs( 1 ) );
+}
+
+TEST_F( ContainerFactoryBoostSharedPtrVectorTest, add_two_base_elements )
+{
+  container_factory::factory<Base, Base>( destinationContainer );
+  
+  ASSERT_THAT( destinationContainer, SizeIs( 2 ) );
+}
+
+class ContainerFactoryUniquePtrVectorTest : public Test
+{
+  protected:
+    using VectorContainer = std::vector<std::unique_ptr<Base>>;
+    
+    void TearDown() override
+    {
+      destinationContainer.clear();
+    }
+    
+    VectorContainer destinationContainer;
+};
+
+TEST_F( ContainerFactoryUniquePtrVectorTest, add_one_base_element )
+{
+  container_factory::factory<Base>( destinationContainer );
+  
+  ASSERT_THAT( destinationContainer, SizeIs( 1 ) );
+}
+
+TEST_F( ContainerFactoryUniquePtrVectorTest, add_two_base_elements )
+{
+  container_factory::factory<Base, Base>( destinationContainer );
+  
+  ASSERT_THAT( destinationContainer, SizeIs( 2 ) );
+}
+
+class ContainerFactorySimpleTypeTest : public Test
+{
+  protected:
+    using VectorContainer = std::vector<Base>;
+    
+    void TearDown() override
+    {
+      destinationContainer.clear();
+    }
+    
+    VectorContainer destinationContainer;
+};
+
+TEST_F( ContainerFactorySimpleTypeTest, add_one_base_element )
+{
+  container_factory::factory<Base>( destinationContainer );
+  
+  ASSERT_THAT( destinationContainer, SizeIs( 1 ) );
+}
+
+TEST_F( ContainerFactorySimpleTypeTest, add_two_base_elements )
+{
+  container_factory::factory<Base, Base>( destinationContainer );
+  
+  ASSERT_THAT( destinationContainer, SizeIs( 2 ) );
+}
+
 #endif // H_625588CDC2DC42A39FF4E11DBF367F15
 
 
