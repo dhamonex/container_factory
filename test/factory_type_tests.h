d832ce472f3fbcc8e8bb6ec65fa1ac2a19f14fc6
Dirk Hartmann <2monex@gmx.net>
2020-11-25 21:12:36 +0100

    Add boost unordered set

--- test/factory_type_tests.h
+++ test/factory_type_tests.h
@@ -36,9 +36,10 @@ using PointerTypes = boost::mp11::mp_list<Base *, std::shared_ptr<Base>, std::un
 
 using VectorContainerTypes = boost::mp11::mp_transform<std::vector, PointerTypes>;
 using ListContainerTypes = boost::mp11::mp_transform<std::list, PointerTypes>;
-using UnorderedSetContanerTypes = boost::mp11::mp_transform<std::unordered_set, PointerTypes>;
+using StdUnorderedSetContainerTypes = boost::mp11::mp_transform<std::unordered_set, PointerTypes>;
+using BoostUnorderedSetContainerTypes = boost::mp11::mp_transform<boost::unordered_set, PointerTypes>;
 
-using ContainerTypes = boost::mp11::mp_append<VectorContainerTypes, ListContainerTypes, UnorderedSetContanerTypes>;
+using ContainerTypes = boost::mp11::mp_append<VectorContainerTypes, ListContainerTypes, StdUnorderedSetContainerTypes, BoostUnorderedSetContainerTypes>;
 using TestContainerTypes = boost::mp11::mp_apply<Types, ContainerTypes>;
 
 TYPED_TEST_SUITE( ContainerFactoryTest, TestContainerTypes );
