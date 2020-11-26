61c0076caad01ed3bdc93445e3d1abb76cd30623
Dirk Hartmann <2monex@gmx.net>
2020-11-26 17:14:51 +0100

    Adjusted test class structure

--- test/test_class_structure.h
+++ test/test_class_structure.h
@@ -5,53 +5,72 @@
 
 class Base
 {
-  public:
-    Base() = default;
-    Base( std::string text )
-      : m_additionalText{ std::move( text ) } 
-    {}
-    
-    virtual ~Base() {};
-    
-    std::string identifierString() const 
-    { 
-      auto identifier = classIdentifierString();
-      
-      if ( !m_additionalText.empty() ) {
-        identifier += "_" + m_additionalText;
-      }
-      
-      return identifier + "_" + std::to_string( m_additionalInteger ); 
+public:
+  Base() = default;
+  Base( std::string text )
+    : m_additionalText{ std::move( text ) }
+  {
+  }
+
+  Base( int number )
+    : m_additionalInteger{ number }
+  {
+  }
+
+  Base( std::string text, int number )
+    : m_additionalText{ std::move( text ) }
+    , m_additionalInteger{ number }
+  {
+  }
+
+  virtual ~Base(){};
+
+  std::string identifierString() const
+  {
+    auto identifier = classIdentifierString();
+
+    if ( !m_additionalText.empty() ) {
+      identifier += "_" + m_additionalText;
     }
-    
-  protected:
-    virtual std::string classIdentifierString() const { return m_className; }
-    
-  private:
-    std::string m_additionalText;
-    int m_additionalInteger{ 0 };
-    
-    inline static const std::string m_className{ "Base" };
+
+    return identifier + "_" + std::to_string( m_additionalInteger );
+  }
+
+protected:
+  virtual std::string classIdentifierString() const
+  {
+    return m_className;
+  }
+
+private:
+  std::string m_additionalText;
+  int m_additionalInteger{ 0 };
+
+  inline static const std::string m_className{ "Base" };
 };
 
 class SubclassA : public Base
 {
-  protected:
-    std::string classIdentifierString() const override { return m_className; }
-      
-  private:
-    inline static const std::string m_className{ "SubclassA" };
+protected:
+  std::string classIdentifierString() const override
+  {
+    return m_className;
+  }
+
+private:
+  inline static const std::string m_className{ "SubclassA" };
 };
 
 class SubclassB : public Base
 {
-  protected:
-    std::string classIdentifierString() const override { return m_className; }
-      
-  private:
-    inline static const std::string m_className{ "SubclassB" };
+protected:
+  std::string classIdentifierString() const override
+  {
+    return m_className;
+  }
+
+private:
+  inline static const std::string m_className{ "SubclassB" };
 };
 
 #endif // H_107F4246F0794C3481C63FF948760E6A
-
-
